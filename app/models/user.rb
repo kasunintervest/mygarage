d8f90c618060bb95a58a57e759757f792e072665
Kasun <kasun@intervest.gi>
2018-02-07 14:47:41 +0530

    master: add validations to register

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :recoverable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
 end
