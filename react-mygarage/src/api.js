df3e7cef8526043a33b21687d45ba29cb7fef01a
kalana <kalana@intervest.gi>
2018-02-23 15:54:21 +0530

    master : stayle changes

--- react-mygarage/src/api.js
+++ react-mygarage/src/api.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
-
+import axiosClient from '../src/axiosClient';
 
 export default {
     user: {
         login: (credentials) =>
-            axios.post(
+            axiosClient.post(
                 'http://localhost:3000/api/v1/sessions',
                credentials,
             ).then(user => user.data),
         signup: user =>
-            axios.post(
-                'http://localhost:3000/api/v1/users.json',
+            axiosClient.post(
+                '/api/v1/users.json',
                 {user},
             ).then(res => res.data),
         update: (id,user) =>
-            axios.put(
-                'http://localhost:3000/api/v1/users.json',
+            axiosClient.put(
+                '/api/v1/users.json',
                 { user,user_email:localStorage.email,user_token:localStorage.mygarageJWT },
             ).then(res => res.data)
     },
 
     vehicles: {
-        fetchAll: () => axios.get("http://localhost:3000/api/v1/vehicles.json?user_email="+localStorage.email+'&user_token='+localStorage.mygarageJWT,).then(res => res),
-        fetch: (id) => axios.get("http://localhost:3000/api/v1/vehicles/"+id+".json?user_email="+localStorage.email+'&user_token='+localStorage.mygarageJWT,).then(res => res),
-        create: vehicle => axios.post("http://localhost:3000/api/v1/vehicles.json", { vehicle,user_email:localStorage.email,user_token:localStorage.mygarageJWT  }).then(res => res.data),
-        update: (id,vehicle) => axios.put("http://localhost:3000/api/v1/vehicles/"+id+".json", { vehicle,user_email:localStorage.email,user_token:localStorage.mygarageJWT  }).then(res => res.data),
-        delete:(id) =>axios.delete("http://localhost:3000/api/v1/vehicles/"+id+".json?user_email="+localStorage.email+'&user_token='+localStorage.mygarageJWT,{ user_email:localStorage.email,user_token:localStorage.mygarageJWT  }).then(res => res.data)
+        fetchAll: () => axiosClient.get("http://localhost:3000/api/v1/vehicles.json?user_email="+localStorage.email+'&user_token='+localStorage.mygarageJWT,).then(res => res),
+        fetch: (id) => axiosClient.get("http://localhost:3000/api/v1/vehicles/"+id+".json?user_email="+localStorage.email+'&user_token='+localStorage.mygarageJWT,).then(res => res),
+        create: vehicle => axiosClient.post("/api/v1/vehicles.json", { vehicle,user_email:localStorage.email,user_token:localStorage.mygarageJWT  }).then(res => res.data),
+        update: (id,vehicle) => axiosClient.put("/api/v1/vehicles/"+id+".json", { vehicle,user_email:localStorage.email,user_token:localStorage.mygarageJWT  }).then(res => res.data),
+        delete:(id) =>axiosClient.delete("/api/v1/vehicles/"+id+".json?user_email="+localStorage.email+'&user_token='+localStorage.mygarageJWT,{ user_email:localStorage.email,user_token:localStorage.mygarageJWT  }).then(res => res.data)
     }
 };
