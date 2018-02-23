95fc0a38ec975e18651cbf3692dd56307c8ab58d
Kasun <kasun@intervest.gi>
2018-02-23 15:40:58 +0530

    master: admin -  manage service records

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!(first_name: 'Admin', last_name: 'P', email: 'admin@mygarage.lk', password: 'admin@mygarage')
-user.add_role :admin
+# user = User.create!(first_name: 'Admin', last_name: 'P', email: 'admin@mygarage.lk', password: 'admin@mygarage')
+# user.add_role :admin
+
+ServiceType.create!(name: 'Free Service', publish: 1)
+ServiceType.create!(name: 'Warranty Repair', publish: 1)
+ServiceType.create!(name: 'General Service', publish: 1)
+ServiceType.create!(name: 'Express Lube', publish: 1)
+ServiceType.create!(name: 'Other', publish: 1)
+
+ServiceCompany.create!(name: 'Toyota Lanka', publish: 1)
+ServiceCompany.create!(name: 'Auto Miraj', publish: 1)
+
