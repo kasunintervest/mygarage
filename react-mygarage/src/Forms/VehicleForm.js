149c3e27dc965345c79a505e04b3b0734032bef9
kalana <kalana@intervest.gi>
2018-02-26 18:32:54 +0530

    master : issue fixes

--- react-mygarage/src/Forms/VehicleForm.js
+++ react-mygarage/src/Forms/VehicleForm.js
@@ -4,7 +4,8 @@ import {Form,Button} from 'semantic-ui-react';
 import InlineError from '../messages/InlineError';
 import {connect} from 'react-redux'
 import { fetchVehicle,updateVehicle } from "../actions/vehicles";
-
+import '../styles/fileUpload.css';
+import axiosClient from '../axiosClient';
 
 class VehicleForm extends React.Component {
 
@@ -18,6 +19,7 @@ class VehicleForm extends React.Component {
             colour: this.props.vehicle ? this.props.vehicle.colour : '',
             year: this.props.vehicle ? this.props.vehicle.year : '',
             details: this.props.vehicle ? this.props.vehicle.details : '',
+            image: this.props.vehicle ? this.props.vehicle.image : '',
         },
         loading:false,
         errors:{
@@ -26,7 +28,12 @@ class VehicleForm extends React.Component {
             make: '',
             model: '',
             year: '',
-        }
+        },
+
+        selectedImageFiles: [],
+        submitFormProgress: 0,
+        isSubmittingForm: false,
+        didFormSubmissionComplete: false,
     };
 
     componentWillReceiveProps = (nextProps) => {
@@ -61,6 +68,64 @@ class VehicleForm extends React.Component {
             data: {...this.state.data,[e.target.name]:e.target.value}
         });
 
+    /*buildFormData(e) {
+        this.state.data.image=this.state.selectedImageFiles[0];
+
+        console.log(this.state.data.image);
+        return this.state.data;
+    }*/
+
+    buildFormData() {
+        let formData = new FormData();
+        formData.append('name', this.state.data.name);
+        formData.append('registration_number', this.state.data.registration_number);
+        formData.append('make', this.state.data.make);
+        formData.append('model', this.state.data.model);
+        formData.append('colour', this.state.data.colour);
+        formData.append('year', this.state.data.year);
+        formData.append('details', this.state.data.details);
+
+
+        /*var file = new File(this.state.selectedImageFiles[0], 'ss.jpeg', {
+            lastModified: new Date(0), // optional - default = now
+            type: "image/jpeg" // optional - default = ''
+        });*/
+
+
+        /*let formData =
+                {
+                    'name': this.state.data.name,
+                    'registration_number':this.state.data.registration_number,
+                    'make':this.state.data.make,
+                    'model':this.state.data.model,
+                    'colour':this.state.data.colour,
+                    'year':this.state.data.year,
+                    'details':this.state.data.details
+                };*/
+
+        //formData['image'] = URL.createObjectURL(this.state.selectedImageFiles[0]);
+
+        /*let { selectedImageFiles } = this.state;
+        for (let i = 0; i < selectedImageFiles.length; i++) {
+            let file = selectedImageFiles[i];
+            if (file.id) {
+                if (file._destroy) {
+                    formData[`[covers_attributes][${i}][id]`] = file.id;
+                    formData[`[covers_attributes][${i}][_destroy]`] = '1';
+                }
+            } else {
+                formData[`[covers_attributes][${i}][photo]`,
+                    file,
+                    file.name
+                );
+            }
+        }*/
+
+        var options = { vehicle: formData };
+
+        return options;
+    }
+
 
     onSubmit = e =>{
         e.preventDefault();
@@ -70,7 +135,6 @@ class VehicleForm extends React.Component {
             loading : true
         });
         if (Object.keys(errors).length === 0) {
-
             if(this.state.data.id != '') {
                 this.props
                     .updateVehicle(this.state.data.id,this.state.data)
@@ -78,6 +142,22 @@ class VehicleForm extends React.Component {
                         this.setState({ errors: !!err.response && !!err.response.data.errors ? err.response.data.errors : {}, loading: false })
                     ).then(()=>this.props.history.push("/vehicles/list"));
             }else {
+
+                var  vehicle = this.state.data;
+
+                /*var vehicle = new FormData(this.form);
+                vehicle.append('sssss', 'dd');*/
+/*
+                var vehicle = { formData };
+*/
+
+               /* axiosClient.post( 'http://localhost:3000/api/v1/vehicles.json', {vehicle,user_email:localStorage.email,user_token:localStorage.mygarageJWT  },{ headers: {
+                    'Content-Type': 'multipart/form-data'
+                }});*/
+
+
+                //console.log(this.buildFormData());
+
                 this.props
                     .submit(this.state.data)
                     .catch(err =>
@@ -102,6 +182,160 @@ class VehicleForm extends React.Component {
 
         return errors;
     }
+
+    /*file upload*/
+
+    getNumberOfSelectedFiles() {
+        return this.state.selectedImageFiles.filter(el => {
+            return el._destroy !== true;
+        }).length;
+    }
+
+    renderUploadCoversButton() {
+        let numberOfSelectedCovers = this.getNumberOfSelectedFiles();
+        return (
+            <div>
+                <input
+                    name="image[]"
+                    ref={field => (this.bookCoversField = field)}
+                    type="file"
+                    disabled={this.state.isSubmittingForm}
+                    multiple={true}
+                    accept="image/*"
+                    style={{
+                        width: 0.1,
+                        height: 0.1,
+                        opacity: 0,
+                        overflow: 'hidden',
+                        position: 'absolute',
+                        zIndex: -1
+                    }}
+                    id="book_covers"
+                    onChange={e => this.handleBookCoversChange(e)}
+                    className="form-control"
+                />
+                <label
+                    disabled={this.state.isSubmittingForm}
+                    className="btn btn-success ui button"
+                    htmlFor="book_covers">
+                    <span className="glyphicon glyphicon-cloud-upload" />
+                    &nbsp; &nbsp;
+                    {numberOfSelectedCovers === 0
+                        ? 'Upload Photos'
+                        : `${numberOfSelectedCovers} file${numberOfSelectedCovers !== 1
+                            ? 's'
+                            : ''} selected`}
+                </label>
+            </div>
+        );
+    }
+
+    renderSelectedBookCoverFiles() {
+        let fileDOMs = this.state.selectedImageFiles.map((el, index) => {
+            if (el._destroy) {
+                return null;
+            }
+
+            return (
+                    <div className="four wide column" key={index+1}>
+
+                        <li key={index}>
+                            <div className="photo">
+                                <img
+                                    width={150}
+                                    src={el.id ? el.url : URL.createObjectURL(el)}
+                                    style={{ alignSelf: 'center' }}
+                                />
+                                <div
+                                    className="remove"
+                                    onClick={() => this.removeSelectedBookCoverFile(el, index)}>
+                                    <span style={{ top: 2 }} className="glyphicon glyphicon-remove ui button" >Remove</span>
+                                </div>
+                            </div>
+                            <div className="file-name">
+                                {el.name} sss
+                            </div>
+                        </li>
+
+
+
+                </div>
+
+
+            );
+        });
+
+        return (
+            <ul className="selected-covers">
+                <div className="ui grid small images">
+                    {fileDOMs}
+                </div>
+            </ul>
+        );
+    }
+
+    renderUploadFormProgress() {
+        if (this.state.loading === false) {
+            return null;
+        }
+
+        return (
+            <div className="progress">
+                <div
+                    className={
+                        'progress-bar progress-bar-info progress-bar-striped' +
+                        (this.state.submitFormProgress < 100 ? 'active' : '')
+                    }
+                    role="progressbar"
+                    aria-valuenow={this.state.submitFormProgress}
+                    min="0"
+                    max="100"
+                    style={{ width: this.state.submitFormProgress + '%' }}>
+                    {this.state.submitFormProgress}% Complete
+                </div>
+            </div>
+        );
+    }
+
+    removeSelectedBookCoverFile(cover, index) {
+        let { selectedImageFiles } = this.state;
+        if (cover.id) {
+            selectedImageFiles[index]._destroy = true;
+        } else {
+            selectedImageFiles.splice(index, 1);
+        }
+
+        this.setState({
+            selectedImageFiles: selectedImageFiles
+        });
+    }
+
+    handleBookCoversChange(e) {
+        let selectedFiles = this.bookCoversField.files;
+        let { selectedImageFiles } = this.state;
+        for (let i = 0; i < selectedFiles.length; i++) {
+            selectedImageFiles.push(selectedFiles.item(i));
+        } //end for
+
+        this.setState(
+            {
+                selectedImageFiles: selectedImageFiles
+            },
+            () => {
+                this.bookCoversField.value = null;
+            }
+        );
+
+        this.setState({
+            ...this.state,
+            data: {...this.state.data,[e.target.name]:'blob:http://localhost:4000/3736f680-cfd3-46b6-b651-e95e89187c3f'}
+        });
+
+
+    }
+
+    /*file upload*/
+
 
     render(){
 
@@ -204,7 +438,18 @@ class VehicleForm extends React.Component {
                         />
                         {errors.details && <InlineError text={errors.details}/>}
                     </Form.Field>
-                    <Button primary>Save</Button>
+
+
+                    <div className="form-group">
+                        <label>Photos</label>
+                        {this.renderUploadCoversButton()}
+                        {this.renderSelectedBookCoverFiles()}
+                    </div>
+                    {this.renderUploadFormProgress()}
+
+
+
+                    <Button primary>{this.state.loading ? 'Saving...' : 'Save'}</Button>
                 </Form>
                 <br/>
             </div>
--- react-mygarage/src/components/Lists/VehicleList.js
+++ react-mygarage/src/components/Lists/VehicleList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import VehicleCard from '../Lists/VehicleCard';
+import { Link } from 'react-router-dom';
+import {Card,Icon} from 'semantic-ui-react';
 
 export default function VehicleList({ vehicles , deleteVehicle  }) {
 
@@ -10,7 +12,24 @@ export default function VehicleList({ vehicles , deleteVehicle  }) {
 
     const vehicleList = (
         <div className="ui four cards">
-            { vehicles.map(vehicle => <VehicleCard vehicle={vehicle} key={vehicle.id} deleteVehicle={deleteVehicle} />) }
+
+            <div className="ui card">
+                <Link to="/vehicles/new">
+                <div class="cards">
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+
+                    <div class="content">
+                        <div class="center aligned description">
+                            <p><Icon name="plus" size="massive" ></Icon></p>
+                        </div>
+                    </div>
+                </div>
+                </Link>
+            </div>
+            { vehicles.length != 0 && vehicles.map(vehicle => vehicle != undefined && <VehicleCard vehicle={vehicle} key={vehicle.id} deleteVehicle={deleteVehicle} />) }
         </div>
     );
 
