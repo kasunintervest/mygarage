ff2af21f0512ba3adc97dca8bfd50ea962348d46
Kasun <kasun@intervest.gi>
2018-02-28 14:50:16 +0530

    master: code refactor, config mysql

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,4 +1,4 @@
-class RegistrationsController < Devise::RegistrationsController
+class Api::V1::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   prepend_before_action :require_no_authentication, only: [:new, :create, :cancel, :update]
@@ -6,7 +6,26 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :authenticate_user_from_token!, :except => [:create]
 
   def create
-    super
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      resource.add_role :customer
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
   end
 
   def update
